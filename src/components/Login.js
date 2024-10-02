541372aca3b4d054b0529d33c273758248d103f4
saritadipa34 <saritadipa34@gmail.com>
2024-10-02 12:12:08 +0530

    form is validated

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,24 @@
 import Header from "./Header";
 import { bgUrl } from "../utils/constant";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { checkValidData } from "../utils/validate";
 const Login=()=>{
     const[isSignInForm,setIsSignInForm]=useState(true);
+
+    const name=useRef(null);
+    const email=useRef(null);
+    const password=useRef(null);
+
+    const [errorMessage,setErrorMessage]=useState(null);
+
+    const handleButtonClick=()=>{
+        console.log(name.current.value);
+        console.log(email.current.value);
+        console.log(password.current.value);
+
+        const message= checkValidData(name.current.value,email.current.value,password.current.value);
+        setErrorMessage(message);
+    }
 
     const toggleSignInForm=()=>{
         setIsSignInForm(!isSignInForm);
@@ -13,15 +29,20 @@ const Login=()=>{
             <div className="absolute">
             <img src={bgUrl} alt="bgimage"/>
             </div>
-                <form className="bg-black text-white  opacity-80 absolute w-4/12 p-14 my-36 mx-auto left-0 right-0">
+                <form onSubmit={(e)=>e.preventDefault()} className="bg-black text-white  opacity-80 absolute w-4/12 p-14 my-36 mx-auto left-0 right-0">
                 <h1 className="font-bold mb-5 text-3xl">{isSignInForm ? "Sign In" :  "Sign Up"}</h1>
-{!isSignInForm && (<input type="text" className="rounded-lg border-2  border-white  p-3.5 my-3 bg-transparent border-solid w-full" placeholder="Name"/>)}
+{!isSignInForm && (<input type="text" ref={name} className="rounded-lg border-2  border-white  p-3.5 my-3 bg-transparent border-solid w-full" placeholder="Name"/>)}
 
-<input type="text" className=" rounded-lg border-2  border-white bg-transparent p-3.5 my-3 w-full" placeholder="Email or mobile number" />
+<input 
+ref={email}
+type="text" className=" rounded-lg border-2  border-white bg-transparent p-3.5 my-3 w-full" placeholder="Email or mobile number" />
 
-<input type="password" className="rounded-lg border-2  border-white  p-3.5 my-3 bg-transparent border-solid w-full" placeholder="Password"/>
+<input type="password" 
+ref={password} className="rounded-lg border-2  border-white  p-3.5 my-3 bg-transparent border-solid w-full" placeholder="Password"/>
 
-<button className="bg-red-600 rounded-lg p-2 my-4 w-full ">Submit</button>
+ <p className="text-red-500 font-bold p-2">{errorMessage}</p>
+
+<button onClick={handleButtonClick} className="bg-red-600 rounded-lg p-2 my-4 w-full ">Submit</button>
 
 <h2 className="text-center">OR</h2>
 
@@ -30,7 +51,7 @@ const Login=()=>{
 <h1 className="text-center">Forgot Password</h1>
 
 <div className="flex my-2">
-    <input className="w-4 mx-2 bg-white text-black" type="checkbox"checked />
+    <input className="w-4 mx-2" type="checkbox" />
     <h1 className="text-l">Remember me</h1>
 </div>
 
---
+++ src/utils/validate.js
@@ -0,0 +1,12 @@
+export const checkValidData=(name,email,password)=>{
+
+    const isNameValid=/^([a-z]+[A-Z])$/.test(name);
+    const isEmailValid=/^([a-z0-9_.-]+)@([da-z.-]+).([a-z.]{2,6})$/.test(email)
+    const IsPasswordValid=/^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{6,})\S$/.test(password)
+
+    if(!isNameValid) return "name is not valid";
+if(!isEmailValid) return "email is not valid";
+if(!IsPasswordValid) return "password is not valid";
+return null;
+
+}
